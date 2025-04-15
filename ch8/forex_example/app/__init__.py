31e0d10f53bcfba57c29282d14b3dd88ad4bcdc2
Mahmoud Fahmy <mahmoudfhamdy@gmail.com>
2025-04-15 18:08:48 +0100

    Delete ch8 directory

--- ch8/forex_example/app/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Initialize the app package.
-This file ensures the directory is treated as a Python package.
-"""
-
-from .chromadb_setup import get_chroma_collection
-from .services import fetch_forex_data_alpha_vantage, generate_embedding, perform_inference_with_phi_model
-from app import get_chroma_collection
